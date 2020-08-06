0bc320331010783aa6722b9cbbac8d3c3bb636c5
John ZuHone <jzuhone@gmail.com>
2020-08-06 16:53:07 -0400

    More renaming

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ import glob
 
 scripts = glob.glob("scripts/*")
 
-setup(name='acispy_tasks',
-      packages=['acispy_tasks'],
+setup(name='acispy_cmd',
+      packages=['acispy_cmd'],
       use_scm_version=True,
       setup_requires=['setuptools_scm', 'setuptools_scm_git_archive'],
-      description='Python tools for ACIS Ops',
+      description='Python-based command-line tools for ACIS Ops',
       author='John ZuHone',
       author_email='john.zuhone@cfa.harvard.edu',
-      url='http://github.com/acisops/acispy_tasks',
+      url='http://github.com/acisops/acispy_cmd',
       install_requires=["numpy>=1.12.1","requests","astropy"],
       scripts=scripts,
       classifiers=[
